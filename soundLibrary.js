57196eaae79502d10ee524f8a51d385068a00503
Michael King <mking385@gmail.com>
2015-08-15 23:54:56 -0400

    add isLoaded check and changed canPlay() to isPlayable()

--- soundLibrary.js
+++ soundLibrary.js
@@ -26,18 +26,6 @@
 
     },
 
-    canPlay: function( soundName ) {
-
-      var sound = this.library[ soundName ] || false;
-
-      //  Sound not in library or in library but no source
-      if( !sound || ( sound && !sound.source ) ) return false;
-
-      //  Sound exists, return standard canPlayType response with more verbose no response than ""
-      return sound.audio.canPlayType( sound.source.format ) || "unknown";
-
-    },
-
     determineBestSource: function( sources ) {
 
       var audioElement = new Audio(),
@@ -66,22 +54,41 @@
 
     },
 
-    play: function( sound ) {
+    isLoaded: function( soundName ) {
+
+      //  Considering ready state of 3 or 4 to mean loaded
+      return this.library[ soundName ].audio.readyState > 2 ? true : false;
+
+    },
+
+    isPlayable: function( soundName ) {
+
+      var sound = this.library[ soundName ] || false;
+
+      //  Sound not in library or in library but no source
+      if( !sound || ( sound && !sound.source ) ) return false;
+
+      //  Sound exists, return standard canPlayType response with more verbose no response than ""
+      return sound.audio.canPlayType( sound.source.format ) || "unknown";
+
+    },
+
+    play: function( soundName ) {
       
       //  Can't play sounds that don't exist
-      if( !this.canPlay( sound ) ) return false;
+      if( !this.isPlayable( soundName ) ) return false;
 
       //  If the sound is not already loaded, load
-      if( !this.library[ sound ].audio.currentSrc ) {
-        this.library[ sound ].audio.src = this.library[ sound ].source.file;
+      if( !this.library[ soundName ].audio.currentSrc ) {
+        this.library[ soundName ].audio.src = this.library[ soundName ].source.file;
       }
       //  If sound is already loaded, set track to beginning
       else {
-        this.library[ sound ].audio.currentTime = 0;
+        this.library[ soundName ].audio.currentTime = 0;
       }
 
       //  Play!
-      this.library[ sound ].audio.play();
+      this.library[ soundName ].audio.play();
 
     }
 
