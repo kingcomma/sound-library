c8ab5a1540aa4954914234265e6d62a3cb547c25
Michael King <mking385@gmail.com>
2015-08-15 23:36:52 -0400

    add preload option

--- soundLibrary.js
+++ soundLibrary.js
@@ -1,12 +1,10 @@
-(function( window ) {
+;(function( window ) {
 
-  var SoundLibrary = function( sounds, settings ) {
+  var SoundLibrary = function( sounds ) {
 
     if( !document.createElement( 'audio' ).canPlayType ) { return; }
 
     this.sounds = sounds;
-    this.settings = settings || {};
-    this.forceSource = this.settings.forceSource || false;
     this.library = {};
     this.init();
 
@@ -16,65 +14,74 @@
 
     init: function() {
 
-      var supported,
-          bestBet;
-
-      //  Container for all playable sounds
-      this.library = {};
-
-      //  Loop through each sound...
+      //  Loop through each sound
       this.sounds.forEach( function( sound ) {
 
-        //  Add sound to library as object containing audio element
-        this.library[ sound.name ] = {
-          audio: new Audio()
-        };
+        //  Add to library
+        this.library[ sound.name ] = {};
+        this.library[ sound.name ].source = this.determineBestSource( sound.sources );
+        this.library[ sound.name ].audio = new Audio( sound.preload ? this.library[ sound.name ].source.file : '' );
 
-        //  If forceSource is set, don't check source support just set it
-        if( this.forceSource && sound.sources.length ) {
-          this.library[ sound.name ].audio.src = sound.sources[0].file;
-        }
-        //  Otherwise, check each source and find most supported
-        else {
-          //  Loop through each possible source for this individual sound...
-          for( var i = 0; i < sound.sources.length; i++ ) {
-            
-            //  Get support level
-            supported = this.library[ sound.name ].audio.canPlayType( sound.sources[i].format );
-
-            //  This sound is supported as well as possible
-            //  Remember it and break loop
-            if( supported === 'probably' ) {
-              bestBet = sound.sources[i].file;
-              break;
-            }
-            //  This sound is probably supported
-            //  Remember it, but keep looping for possible better match
-            else if ( supported === 'maybe' ) {
-              bestBet = sound.sources[i].file;
-            }
-          }
-          
-          //  Set audio element's source to bestBet
-          this.library[ sound.name ].audio.src = bestBet;
-        }
-  
       }, this );
 
     },
 
-    canPlay: function( sound ) {
+    canPlay: function( soundName ) {
 
-      return this.library[ sound ] && this.library[ sound ].currentSrc !== "" ? true : false;
+      var sound = this.library[ soundName ] || false;
+
+      //  Sound not in library or in library but no source
+      if( !sound || ( sound && !sound.source ) ) return false;
+
+      //  Sound exists, return standard canPlayType response with more verbose no response than ""
+      return sound.audio.canPlayType( sound.source.format ) || "unknown";
+
+    },
+
+    determineBestSource: function( sources ) {
+
+      var audioElement = new Audio(),
+          supported,
+          bestBet;
+
+      for( var i = 0; i < sources.length; i++ ) {
+        
+        //  Get support level
+        supported = audioElement.canPlayType( sources[i].format );
+
+        //  This sound is supported as well as possible
+        //  Remember it and break loop
+        if( supported === 'probably' ) {
+          bestBet = sources[i];
+          break;
+        }
+        //  This sound is probably supported
+        //  Remember it, but keep looping for possible better match
+        else if ( supported === 'maybe' ) {
+          bestBet = sources[i];
+        }
+      }
+      
+      return bestBet;
 
     },
 
     play: function( sound ) {
       
-      if( this.canPlay( sound ) ) {
-        this.library[ sound ].audio.currentTime = 0;
-        this.library[ sound ].audio.play();
+      //  Can't play sounds that don't exist
+      if( !this.canPlay( sound ) ) return false;
+
+      //  If the sound is not already loaded, load
+      if( !this.library[ sound ].audio.currentSrc ) {
+        this.library[ sound ].audio.src = this.library[ sound ].source.file;
       }
+      //  If sound is already loaded, set track to beginning
+      else {
+        this.library[ sound ].audio.currentTime = 0;
+      }
+
+      //  Play!
+      this.library[ sound ].audio.play();
 
     }
 
